448de04f66b0226b9d1e71a518e2da29c5cb069e
danieceiflora <daniel_sousa_marques@hotmail.com>
2025-01-15 11:48:35 -0300

    atualizacao de configuracao de jwt

--- app/settings.py
+++ app/settings.py
@@ -139,8 +139,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=365 * 5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=365 * 5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 JAZZMIN_SETTINGS = {
